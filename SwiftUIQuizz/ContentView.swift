50943e4b2ebbcbce017b05e69012010f8bdb5860
Vitor Moriya <vitorjmoriya@gmail.com>
2022-05-04 09:22:12 -0300

    Initial Commit

---
+++ SwiftUIQuizz/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIQuizz
+//
+//  Created by Vitor Jundi Moriya on 04/05/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUIQuizz/SwiftUIQuizzApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIQuizzApp.swift
+//  SwiftUIQuizz
+//
+//  Created by Vitor Jundi Moriya on 04/05/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIQuizzApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
