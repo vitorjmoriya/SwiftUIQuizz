f0b5ef0568421585463b9d9a2dfacd1f5dcb9b1c
Henrique Loureiro de Faria <h217790@dac.unicamp.br>
2022-05-10 10:50:15 -0300

    feat(Button): add ButtonView

---
+++ SwiftUIQuizz/Views/ButtonView.swift
@@ -0,0 +1,43 @@
+//
+//  ButtonView.swift
+//  SwiftUIQuizz
+//
+//  Created by Henrique Loureiro de Faria on 10/05/22.
+//
+
+import SwiftUI
+import LottieSwiftUI
+
+extension Views {
+    struct Button: View {
+        @Binding var isAnimating: Bool
+        let isCorrect: Bool // True -> correct answer -- False -> wrong answer
+        let buttonText: String
+        init(
+            isAnimating: Binding<Bool>,
+            isCorrect: Bool,
+            buttonText: String
+        ) {
+            self.isCorrect = isCorrect
+            self.buttonText = buttonText
+            self._isAnimating = isAnimating
+        }
+        var body: some View {
+            SwiftUI.Button(
+                action: { isAnimating = true}
+            ) {
+                HStack(spacing: 10.0) {
+                    Text(buttonText).padding(.leading)
+                    if isAnimating {
+                        LottieView(name: isCorrect ? "correct": "wrong")
+                            .lottieLoopMode(.playOnce)
+                            .frame(width: 30.0,
+                                   height: 30.0,
+                                   alignment: Alignment.trailing)
+                    }
+                }
+                .padding()
+            }
+        }
+    }
+}
