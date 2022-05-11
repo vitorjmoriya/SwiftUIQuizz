52b3a1e3af24a911a0a5f6ce73a5c3b7fe84c714
Vitor Moriya <vitorjmoriya@gmail.com>
2022-05-11 09:17:28 -0300

    feat(Views): add Initial Profile view

---
+++ SwiftUIQuizz/Views/ProfileView.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileView.swift
+//  SwiftUIQuizz
+//
+//  Created by Vitor Jundi Moriya on 11/05/22.
+//
+
+import SwiftUI
+import LottieSwiftUI
+
+extension Views {
+    struct Profile: View {
+        
+        var body: some View {
+            Text("Aqui era para ter uma tela de perfil")
+        }
+    }
+}
