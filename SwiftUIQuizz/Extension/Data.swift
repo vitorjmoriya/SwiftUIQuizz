1046100e4586c282752563dbd8e803eeef36993a
Vitor Moriya <vitorjmoriya@gmail.com>
2022-05-24 09:36:18 -0300

    refactor(Swiftlint): fix errors

--- SwiftUIQuizz/Extension/Data.swift
+++ SwiftUIQuizz/Extension/Data.swift
@@ -10,7 +10,14 @@ import Foundation
 extension Data {
     var html2AttributedString: NSAttributedString? {
         do {
-            return try NSAttributedString(data: self, options: [.documentType: NSAttributedString.DocumentType.html, .characterEncoding: String.Encoding.utf8.rawValue], documentAttributes: nil)
+            return try NSAttributedString(
+                data: self,
+                options: [
+                    .documentType: NSAttributedString.DocumentType.html,
+                    .characterEncoding: String.Encoding.utf8.rawValue
+                ],
+                documentAttributes: nil
+            )
         } catch {
             print("error:", error)
             return  nil
