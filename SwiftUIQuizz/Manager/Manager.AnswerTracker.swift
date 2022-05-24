143332dc232f16836dddaa06d956ef99b1bd1e3d
Henrique Finger Zimerman <henfinzim@gmail.com>
2022-05-24 11:39:49 -0300

    basic question tracker

---
+++ SwiftUIQuizz/Manager/Manager.AnswerTracker.swift
@@ -0,0 +1,38 @@
+//
+//  Manager.Session.swift
+//  SwiftUIQuizz
+//
+//  Created by Henrique Finger Zimerman on 24/05/22.
+//
+
+import Foundation
+
+extension Manager {
+    class AnswerTracker {
+        public static let shared = AnswerTracker()
+
+        var questionAmount: Int
+        var correctAnswers: Int
+        var wrongAnswers: Int
+
+        private init() {
+            self.questionAmount = 0
+            self.correctAnswers = 0
+            self.wrongAnswers = 0
+        }
+
+        func startQuiz(questionAmount: Int = 0) {
+            self.questionAmount = questionAmount
+            self.correctAnswers = 0
+            self.wrongAnswers = 0
+        }
+
+        func addResult(answerStatus: Bool) {
+            if answerStatus {
+                correctAnswers += 1
+            } else {
+                wrongAnswers += 1
+            }
+        }
+    }
+}
